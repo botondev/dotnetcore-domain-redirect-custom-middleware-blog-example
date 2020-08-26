eac460545818901c6166cf93dccea63b0f1dd562
Botond Bertalan <botondev.hu@gmail.com>
2020-08-26 09:39:21 +0200

    RedirectToPublicDomainConfiguration class for options pattern

---
+++ dotnetcore-domain-redirect-custom-middleware-blog-example/Configuration/RedirectToPublicDomainConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_domain_redirect_custom_middleware_blog_example.Configuration
+{
+    public class RedirectToPublicDomainConfiguration
+    {
+        public string RedirectFromHost { get; set; }
+
+        public string RedirectToHost { get; set; }
+
+        public bool IsPermanent { get; set; }
+    }
+}
